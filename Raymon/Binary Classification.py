491d91c669ca737efb2e69a98682d1be0deb8aca
Tal888 <43509186+Tal888@users.noreply.github.com>
2020-04-27 12:48:22 +1000

    added code for making cv graphs

--- Raymon/Binary Classification.py
+++ Raymon/Binary Classification.py
@@ -203,3 +203,87 @@ with open('df_val_predicted_relevant.pickle', 'wb') as output:
 with open('df_val.pickle', 'wb') as output:
     pickle.dump(df_val, output)
 
+    
+### Creation of Graphs for Report ###
+
+# Reference:
+# https://scikit-learn.org/stable/auto_examples/model_selection/plot_multi_metric_evaluation.html
+# Plotting Multiple metrics for Kfold (k=5) cross validation on a feature
+
+### Plotting Multiple Metrics at same time for Max Features
+results = grid_search_tune.cv_results_
+scoring = {'AUC': 'roc_auc', 'F1': 'f1', 'Precision': 'precision', 'Recall': 'recall'}
+
+plt.figure(figsize=(13, 13))
+plt.title("GridSearchCV evaluating using multiple scorers simultaneously",
+          fontsize=16)
+
+plt.xlabel("Max features")
+plt.ylabel("Score")
+
+ax = plt.gca()
+ax.set_xlim(0, 10000)
+ax.set_ylim(0.73, 1)
+
+# Get the regular numpy array from the MaskedArray
+X_axis = np.array(results['param_tfidf__max_features'].data, dtype=float)
+
+for scorer, color in zip(sorted(scoring), ['g', 'k', 'y','r']):
+    for sample, style in (('train', '--'), ('test', '-')):
+        sample_score_mean = results['mean_%s_%s' % (sample, scorer)]
+        sample_score_std = results['std_%s_%s' % (sample, scorer)]
+        ax.fill_between(X_axis, sample_score_mean - sample_score_std,
+                        sample_score_mean + sample_score_std,
+                        alpha=0.1 if sample == 'test' else 0, color=color)
+        ax.plot(X_axis, sample_score_mean, style, color=color,
+                alpha=1 if sample == 'test' else 0.7,
+                label="%s (%s)" % (scorer, sample))
+
+    best_index = np.nonzero(results['rank_test_%s' % scorer] == 1)[0][0]
+    best_score = results['mean_test_%s' % scorer][best_index]
+
+    # Plot a dotted vertical line at the best score for that scorer marked by x
+    ax.plot([X_axis[best_index], ] * 2, [0, best_score],
+            linestyle='-.', color=color, marker='x', markeredgewidth=3, ms=8)
+
+    # Annotate the best score for that scorer
+    ax.annotate("%0.2f" % best_score,
+                (X_axis[best_index], best_score + 0.005))
+
+plt.legend(loc="best")
+plt.grid(False)
+plt.show()
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+min_df = (1,5,10,20)
+max_features = (100,500,1000,2000,5000,10000)
+
+
+# Reference for plot_grid_search:
+# https://stackoverflow.com/questions/37161563/how-to-graph-grid-scores-from-gridsearchcv
+# User - Mike Lewis
+def plot_grid_search(cv_results, grid_param_1, grid_param_2, name_param_1, name_param_2):
+    # Get Test Scores Mean and std for each grid search
+    scores_mean = cv_results['mean_test_score']
+    scores_mean = np.array(scores_mean).reshape(len(grid_param_2),len(grid_param_1))
+
+    scores_sd = cv_results['std_test_score']
+    scores_sd = np.array(scores_sd).reshape(len(grid_param_2),len(grid_param_1))
+
+    # Plot Grid search scores
+    _, ax = plt.subplots(1,1)
+
+    # Param1 is the X-axis, Param 2 is represented as a different curve (color line)
+    for idx, val in enumerate(grid_param_2):
+        ax.plot(grid_param_1, scores_mean[idx,:], '-o', label= name_param_2 + ': ' + str(val))
+
+    ax.set_title("Grid Search Scores", fontsize=20, fontweight='bold')
+    ax.set_xlabel(name_param_1, fontsize=16)
+    ax.set_ylabel('CV Average ROC_AUC Score', fontsize=16)
+    ax.legend(loc="best", fontsize=15)
+    ax.grid('on')
+# Calling Method 
+plot_grid_search(grid_search_tune.cv_results_, max_features, min_df, 'Max Features', 'min_df')
